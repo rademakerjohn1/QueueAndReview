a3cd075755bb1af52449813b9a735734d20e855f
John Rademaker <rademakerjohn1@gmail.com>
2020-08-13 11:36:51 -0400

    Changed label tags' "for" attributes" to "htmlFor"

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -6,19 +6,19 @@ function Register() {
     return (
         <form id="register" action="/user/register" method="POST">
             <div className="form-group">
-                <label for="name">Name</label>
+                <label htmlFor="name">Name</label>
                 <input type="text" className="form-control" id="name" name="name" placeholder="Enter name" />
             </div>
             <div className="form-group">
-                <label for="email">Email address</label>
+                <label htmlFor="email">Email address</label>
                 <input type="email" className="form-control" id="email" name="email" placeholder="Enter email" />
             </div>
             <div className="form-group">
-                <label for="password">Password</label>
+                <label htmlFor="password">Password</label>
                 <input type="password" className="form-control" id="password1" name="password" placeholder="Password" />
             </div>
             <div className="form-group">
-                <label for="password2">Confirm Password</label>
+                <label htmlFor="password2">Confirm Password</label>
                 <input type="password" className="form-control" id="password2" name="password2" placeholder="Password" />
             </div>
             <Link to="/login">Already registered? Log in.</Link>
