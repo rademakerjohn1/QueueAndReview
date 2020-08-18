8c5a46b36b835e1ac7560a39f88d7ba763ec9608
John Rademaker <rademakerjohn1@gmail.com>
2020-08-18 15:58:13 -0400

    test

--- server.js
+++ server.js
@@ -45,9 +45,8 @@ app.use(morgan('tiny'));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build", index.html)); // <- try "index.html"
-  });
-}
+    res.sendFile(path.join(__dirname, "/client/build/index.html"))
+})}
 
 app.listen(PORT, (err) => {
     if (err) throw console.log(err);
