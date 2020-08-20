f022801cad09f348d8e6501a7acc6c0c19fb996c
John Rademaker <rademakerjohn1@gmail.com>
2020-08-20 16:45:43 -0400

    added styling for active nav links

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
 import './Nav.css'
 
 // Render Navigation section with Search, Queue, and Library components
 
 function Navigation() {
+
+  let pathname = window.location.pathname;
+
   return (
       <Nav>
-        <NavItem>
+        <NavItem className={pathname === "/search" && "active"}>
             <NavLink href="/search">Search</NavLink> 
         </NavItem>
-        <NavItem>
+        <NavItem className={(pathname === "/queue" || pathname === "/") && "active"}>
             <NavLink href="/queue">Queue</NavLink>
         </NavItem>
-        <NavItem>
+        <NavItem className={pathname === "/library" && "active"}>
             <NavLink href="/library">Library</NavLink>
         </NavItem>
       </Nav>
