9c3b3a0d055a3b3a087ee69af86df746ee249275
John Rademaker <rademakerjohn1@gmail.com>
2020-08-21 13:32:19 -0400

    adjusting nav link styling

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -10,13 +10,13 @@ function Navigation() {
 
   return (
       <Nav>
-        <NavItem className={pathname === "/search" && "active"}>
+        <NavItem className={pathname === "/search" ? "active" : null}>
             <NavLink href="/search">Search</NavLink> 
         </NavItem>
-        <NavItem className={(pathname === "/queue" || pathname === "/") && "active"}>
+        <NavItem className={(pathname === "/queue" || pathname === "/") ? "active" : null}>
             <NavLink href="/queue">Queue</NavLink>
         </NavItem>
-        <NavItem className={pathname === "/library" && "active"}>
+        <NavItem className={pathname === "/library" ? "active" : null}>
             <NavLink href="/library">Library</NavLink>
         </NavItem>
       </Nav>
